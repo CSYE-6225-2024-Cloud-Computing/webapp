6b04b66151ab0d261364b388f2c18f9831480f55
Shreya Jaiswal <shreyajaiswal1604@gmail.com>
2024-02-20 22:18:06 -0500

    packer-test-it-18

---
+++ packer/webapp_startup.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Install unzip utility 
+sudo dnf install -y unzip
+
+# Create the target directory if it doesn't exist
+sudo mkdir -p /home/csye6225/webapp
+
+# Install application dependencies and copy artifacts and configuration files
+sudo unzip /tmp/webapp.zip -d /home/csye6225/webapp
+
+echo '================================================================================================================================================'
+echo 'Unzipping Completed.'
+echo '================================================================================================================================================'
+
+
+# User Creation Installation
+sudo chmod +x /home/csye6225/webapp/packer/userInstall.sh
+sudo /home/csye6225/webapp/packer/userInstall.sh
+
+# Copy service file to the correct location
+sudo cp /home/csye6225/webapp/service/webapp.service /etc/systemd/system/webapp.service
+sudo chown csye6225:csye6225 /etc/systemd/system/webapp.service
+sudo chmod 550 /etc/systemd/system/webapp.service
+
+# Database Installation
+sudo chmod +x /home/csye6225/webapp/packer/databaseInstall.sh
+sudo /home/csye6225/webapp/packer/databaseInstall.sh
+
+# Python Installation
+sudo chmod +x /home/csye6225/webapp/packer/pythonInstall.sh
+sudo /home/csye6225/webapp/packer/pythonInstall.sh
+
+# Pip Requirements Installation
+sudo pip3.9 install -r /home/csye6225/webapp/app/requirements.txt 
+
+echo '================================================================================================================================================'
+echo 'Requirements.txt installations completed.'
+echo '================================================================================================================================================'
+
+# Give ownership permission to csye6225
+sudo chown -R csye6225:csye6225 /home/csye6225/webapp/
+
+echo '================================================================================================================================================'
+echo 'Ownership of the application directory set to the dedicated user - csye6225'
+echo '================================================================================================================================================'
+
+# Reload systemd to pick up changes
+sudo systemctl daemon-reload
+
+# Enable the webapp.service to start on boot
+sudo systemctl enable webapp
+
+# Start the webapp.service
+sudo systemctl start webapp
+
+echo '================================================================================================================================================'
+echo 'Ownership of the application directory set to the dedicated user - csye6225'
+echo '================================================================================================================================================'
+echo '================================================================================================================================================'
+echo 'Custom image setup completed'
+echo '================================================================================================================================================'
