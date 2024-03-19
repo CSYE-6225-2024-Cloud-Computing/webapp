15a22796f32ee4481513a7e6f40e49763b8cb1e7
Shreya Jaiswal <shreyajaiswal1604@gmail.com>
2024-03-19 13:38:00 -0400

    assignment-06-v5

--- app/hashing.py
+++ app/hashing.py
@@ -1,11 +1,33 @@
 
 from passlib.context import CryptContext
+import logging
+from pythonjsonlogger import jsonlogger
+
+# Initialize logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create JSON handler with log file path
+log_file_path = '/var/log/webapp.log'  # Specify the log file path here
+logHandler = logging.FileHandler(log_file_path)
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
 
 pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Hash():
     def bcrypt(password: str):
+        logger.info("Password hashed successfully")
         return pwd_cxt.hash(password)
     
+    # def verify(hashed_password, plain_password):
+    #     return pwd_cxt.verify(plain_password,hashed_password)
+
     def verify(hashed_password, plain_password):
-        return pwd_cxt.verify(plain_password,hashed_password)
+        is_verified = pwd_cxt.verify(plain_password, hashed_password)
+        if is_verified:
+            logger.info("Password verification successful")
+        else:
+            logger.warning("Password verification failed")
+        return is_verified
--- app/routers/healthz.py
+++ app/routers/healthz.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Request, Response, HTTPException, APIRouter
 from fastapi.responses import JSONResponse
+import logging
+from pythonjsonlogger import jsonlogger
+
 
 
 router = APIRouter(
@@ -7,15 +10,27 @@ router = APIRouter(
     tags=['healthz']
 )
 
+# Initialize logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Create JSON handler with log file path
+log_file_path = '/var/log/webapp.log'  # Specify the log file path here
+logHandler = logging.FileHandler(log_file_path)
+formatter = jsonlogger.JsonFormatter()
+logHandler.setFormatter(formatter)
+logger.addHandler(logHandler)
+
 
 @router.get("/")
 async def check_database_connection(request: Request):
     # TODO: Add a return code for 503
     data = await request.body()
     if request.query_params or data:
-        print("hello")
+        logger.warning("Unexpected query parameters or body data received")
         return Response(status_code=400, headers={"Cache-Control": "no-cache"})
     else:
+        logger.info("Health check endpoint accessed successfully")
         return Response(status_code=200, headers={"Cache-Control": "no-cache"})
 
 @router.post("/")
@@ -25,5 +40,6 @@ async def check_database_connection(request: Request):
 @router.head("/")
 @router.options("/")
 async def disallowed_methods(request: Request):
+    logger.warning("Disallowed HTTP method called")
     return Response(status_code=405)
 
